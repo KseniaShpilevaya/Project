9226ee5b2feb0a8c5822820b8b0fc68a5b667d78
KseniaShpilevaya <vo_mne_ymer_poet@mail.ru>
2023-09-11 18:07:09 +0200

    считывание информации

--- Project/Solution.cpp
+++ Project/Solution.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
 
-int main() {
-    std::cout << "Hello, world!\n";
+using namespace std;
+
+// функция, которая считывает данные из файла txt
+
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string str;
+    ifstream fin;
+    
+    fin.open("input.txt");
+    while (getline(fin, str))
+    {
+        cout << str << endl;
+    }
+    fin.close();
+    
     return 0;
 }
